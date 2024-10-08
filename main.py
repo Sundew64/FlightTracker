726a3f74c45e42bb9c652c979a2ae189cd66869a
Sundew64 <guptaihita3@gmail.com>
2024-10-08 22:32:06 +0000

    first commit

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # step 1: import requests
+import requests
 
 # step 2: call api with given endpoint and key
+api_result = requests.get() # requests the data from the endpoint
 
 # step 3: convert to dictionary and grab only the "data" value
+api_result=api_result.json()
+data = api_result['data']
 
 # step 4: loop through the flights and check which flights are active
 
--- solution.py
+++ solution.py
@@ -1,6 +1,8 @@
 import requests # import requests library for use
+from dotenv import dotenv_values
+dotenv=dotenv_values('.env')
 
-api_result = requests.get('https://api.aviationstack.com/v1/flights?access_key=705dfe02cd2777e1723680be7d9ca4f1') # requests the data from the endpoint
+api_result = requests.get(dotenv['API']) # requests the data from the endpoint
 # to add other parameters you can add "&parameter=value&parameter2=value2" etc.
 
 # Or you can do it this way
